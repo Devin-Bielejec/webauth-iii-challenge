222e7fb434117ca80d44163a8e8ef7189b75a217
devin-bielejec <devin.bieleje@gmail.com>
2019-10-02 15:16:27 -0400

    Wrote knex functions

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,22 @@
-// Update with your config settings.
-
-module.exports = {
-
+const db = require("knex")({
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
     }
   }
-  
-};
+})
+
+const findAll = () => {
+  return db("users");
+}
+
+const insert = (user) => {
+  return db("users")
+  .insert(user);
+}
+
+const findByUserName = (username) => {
+  return db("users")
+  .where(username);
+}
